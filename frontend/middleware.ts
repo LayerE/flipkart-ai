9362ea0a33676e0e1c23858755911cacbfd8bf73
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2023-09-10 06:51:16 +0000

    Add Canvas Save and Fetch Route

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -4,9 +4,14 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-
-  publicRoutes: ["/api/generate", "/api/removebg", "/api/hello", "/api/images", "/api/user"],
-
+  publicRoutes: [
+    "/api/generate",
+    "/api/removebg",
+    "/api/hello",
+    "/api/images",
+    "/api/user",
+    "/api/canvasdata",
+  ],  
 });
 
 export const config = {
