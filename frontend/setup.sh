0a6bffca9c3555a3137f97b809e00e00f457547b
nisamozr <codeexplorkl10@gmail.com>
2023-10-25 02:39:49 +0530

    op

--- frontend/setup.sh
+++ frontend/setup.sh
@@ -14,8 +14,8 @@ tar -xf zlib-1.2.9.tar.gz
 cd zlib-1.2.9
 sh configure
 make
-cp libz.so.1.2.9 ../node_modules/canvas/build/Release/libz.so.X
+cp libz.so.1.2.9 ../node_modules/fabric/node_modules/canvas/build/Release/libz.so.X
 cd ..
 
-patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
-patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/canvas/build/Release/libpng16.so.16
+patchelf --replace-needed /lib64/libz.so.1 libz.so.X ./node_modules/fabric/node_modules/canvas/build/Release/libpng16.so.16
+patchelf --replace-needed libz.so.1 libz.so.X ./node_modules/fabric/node_modules/canvas/build/Release/libpng16.so.16
