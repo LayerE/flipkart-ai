646f8f4ca5332e994eca3c194ac4dc7a4d5f5740
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2023-11-18 14:29:53 +0000

    Migrate BG Remover

--- frontend/pages/api/upload.ts
+++ frontend/pages/api/upload.ts
@@ -1,9 +1,10 @@
 // @ts-nocheck
 
 import { NextResponse, NextRequest } from "next/server";
-import axios from "axios";
 import FormData from "form-data";
-const { createCanvas, loadImage } = require("canvas");
+import { v4 as uuidv4 } from "uuid";
+import { createClient } from "@supabase/supabase-js";
+
 export const maxDuration = 300;
 
 export const config = {
@@ -14,51 +15,90 @@ export const config = {
   },
 };
 
-// async function getImageDimensions(base64Url: string) {
-//   const canvas = createCanvas();
-//   const ctx = canvas.getContext("2d");
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.SUPABASE_SERVICE_KEY as string
+);
 
-//   try {
-//     const image = await loadImage(base64Url);
-//     canvas.width = image.width;
-//     canvas.height = image.height;
-//     ctx.drawImage(image, 0, 0);
-//     const dimensions = {
-//       width: canvas.width,
-//       height: canvas.height,
-//     };
-//     return dimensions;
-//   } catch (error) {
-//     console.error("Error:", error.message);
-//     return { error: error.message };
-//   }
-// }
+const getFileExtension = (dataUrl: string) => {
+  if (dataUrl.includes("png")) {
+    return "png";
+  } else if (dataUrl.includes("jpg") || dataUrl.includes("jpeg")) {
+    return "jpg";
+  } else if (dataUrl.includes("webp")) {
+    return "webp";
+  } else {
+    // Default extension or handle other cases as needed
+    return "png";
+  }
+};
 
-const uploadImage = async (dataUrl: string) => {
-  const formdata = new FormData();
-  formdata.append("file", dataUrl);
-  formdata.append("fileName", "img.png");
+const uploadImage = async (
+  dataUrl: string,
+  user_id?: string,
+  useImageKit: boolean = false
+) => {
+  if (useImageKit) {
+    const formdata = new FormData();
+    formdata.append("file", dataUrl);
+    formdata.append("fileName", "img.png");
 
-  try {
-    const response = await axios.post(
+    const imageKitResponse = await fetch(
       "https://upload.imagekit.io/api/v1/files/upload",
-      formdata,
       {
+        method: "POST",
         headers: {
           Authorization: "Basic " + btoa(process.env.IMAGEKIT_API_KEY + ":"),
-          ...formdata.getHeaders(),
         },
+        body: formdata,
       }
     );
 
-    const { data } = response;
-    const { url, name, height, width } = data;
+    const imageKitJson = await imageKitResponse.json();
+    const { url, name, height, width } = imageKitJson;
 
-    return { url, name, height, width };
-  } catch (error) {
-    // Handle errors here
-    console.error("Error uploading image:", error);
-    throw error;
+    return {
+      url: url + "?tr=orig-true",
+      name: name,
+      height: height,
+      width: width,
+    };
+  } else {
+    var base64String = dataUrl;
+    if (!dataUrl.includes("data:image")) {
+      base64String = `data:image/png;base64,${dataUrl}`;
+    }
+
+    // Generate a unique filename
+    const filename = `${user_id}/${uuidv4()}.${getFileExtension(base64String)}`;
+    const bucket_name =
+      process.env.SUPABASE_REQUEST_IMAGES_BUCKET || "request_images";
+
+    const byteCharacters = atob(base64String.split(",")[1]);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    const blob = new Blob([byteArray], { type: "image/png" });
+
+    // Upload image to supabase storage bucket
+    const { error } = await supabase.storage
+      .from(bucket_name)
+      .upload(`${filename}`, blob, {
+        cacheControl: "public, max-age=31536000, immutable",
+        upsert: false,
+      });
+
+    if (error) {
+      console.log(error.message);
+      throw error;
+    }
+
+    return {
+      url: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${bucket_name}/${filename}`,
+      name: filename,
+    };
   }
 };
 
@@ -72,7 +112,6 @@ export default async function handler(req: NextRequest, res: NextResponse) {
     const { body } = req;
     const payload = body;
     const { user_id, dataUrl, project_id, type } = payload;
-    console.log(dataUrl);
 
     if (!user_id) {
       res.status(400).send("Missing user_id");
@@ -85,27 +124,9 @@ export default async function handler(req: NextRequest, res: NextResponse) {
       image_type = type;
     }
 
-    const inputBase64Url = dataUrl;
-
-    // Get the image dimensions of the image from its base64Url
-    // const { width: img_width, height: img_height } = await getImageDimensions(
-    //   inputBase64Url
-    // );
-
-    // if (!img_width || !img_height) {
-    //   res.status(400).send("Image is corrupted or unsupported dimensions");
-    //   return;
-    // }
-
-    // // Check if image is less than 25MP
-    // if (img_width * img_height > 25000000) {
-    //   res.status(400).send("Image is too big");
-    //   return;
-    // }
-
     // Upload image to ImageKit
-    const { url: imageUrl, height, width } = await uploadImage(dataUrl);
-    console.log(imageUrl)
+    const { url: imageUrl } = await uploadImage(dataUrl, user_id, false);
+    console.log(imageUrl);
 
     if (image_type == "image") {
       // Add the image to the database
@@ -155,7 +176,7 @@ export default async function handler(req: NextRequest, res: NextResponse) {
     return;
   } catch (error) {
     console.log(error);
-    res.status(500).send("Error removing background");
+    res.status(500).send("Error uploading image");
     return;
   }
 }
