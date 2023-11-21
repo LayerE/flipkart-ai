433552ac9cf1f7d5062e137b21852efb4b0fff7a
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2023-11-21 13:30:32 +0000

    Fix regenerate flow

--- frontend/pages/api/images.ts
+++ frontend/pages/api/images.ts
@@ -19,12 +19,12 @@ export default async (req: NextRequest) => {
       return NextResponse.json({ error: "Missing user_id" });
     }
 
-    const postURL =
+    const requestURL =
       project_id != null
-        ? `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/${process.env.NEXT_PUBLIC_BACKGROUND_REMOVED_IMAGES_TABLE}?select=caption,image_url,project_id&user_id=eq.${user_id}&project_id=eq.${project_id}`
-        : `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/${process.env.NEXT_PUBLIC_BACKGROUND_REMOVED_IMAGES_TABLE}?select=caption,image_url,project_id&user_id=eq.${user_id}`;
+        ? `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/${process.env.NEXT_PUBLIC_BACKGROUND_REMOVED_IMAGES_TABLE}?select=caption,image_url,project_id&user_id=eq.${user_id}&project_id=eq.${project_id}&is_regenerated=eq.false`
+        : `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/${process.env.NEXT_PUBLIC_BACKGROUND_REMOVED_IMAGES_TABLE}?select=caption,image_url,project_id&user_id=eq.${user_id}&is_regenerated=eq.false`;
 
-    const response = await fetch(postURL, {
+    const response = await fetch(requestURL, {
       headers: {
         apikey: process.env.SUPABASE_SERVICE_KEY as string,
         Authorization: `Bearer ${process.env.SUPABASE_SERVICE_KEY}` as string,
